12a4b2fd20426b081e74a4994c45c60f3eb9c8fe
BugHunter-X <askzenai@gmail.com>
2025-02-08 11:16:39 +0100

    Add AppSolution.js file

---
+++ AppSolution.js
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
